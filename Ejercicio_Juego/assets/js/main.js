08d518ba570651e573d29e839714b0ef37b7a7d7
ernesto <ernestogba2@gmail.com>
2026-04-01 20:48:27 +0200

    Mejoras visuales y mejoras de compresion de codigo. Ernesto casa

--- Ejercicio_Juego/assets/js/main.js
+++ Ejercicio_Juego/assets/js/main.js
@@ -7,45 +7,8 @@ const jugador = {
   ataque: 15,
   pociones: 3,
   clase: "Arquero",
+};
 
-  caracteristicas: function () {
-    console.log(
-      `| Nombre: ${this.nombre} | Clase: ${this.clase} |
-       |Ataque: ${this.ataque} | Vida: ${this.vida} |`,
-    );
-  },
-
-  //Funcion para cambiar datos del personaje
-  subirNivel: function () {
-    this.ataque += 5;
-    console.log(`Subiste de nivel por tanto has ganado: ${this.ataque}`);
-
-    escribirEnLog(`${jugador.nombre} ha subido ${jugador.nivel} niveles`);
-  },
-
-
-  atacar: function (enemigo) {
-    //Restamos vida al enemigo
-    enemigo.vida -= jugadorActual.ataque;
-
-    if (enemigo.vida <= 0) {
-      enemigo.vida = 0;
-
-      escribirEnLog(`¡${jugadorActual.nombre} ha derrotado a ${enemigo.nombre}! 🏆`);
-      const vidaEnemigoTexto = document.getElementById("vida-enemigo");
-      vidaEnemigoTexto.innerText = enemigo.vida;
-
-    } else {
-
-      const vidaEnemigoTexto = document.getElementById("vida-enemigo");
-      vidaEnemigoTexto.innerText = enemigo.vida;
-      escribirEnLog(`Al slime le quedan: ${enemigo.vida} puntos de vida`);
-    }
-
-  }
-}
-
-//Objeto enemigo
 const jugador2 = {
   nombre: "Arturo",
   nivel: 5,
@@ -53,25 +16,7 @@ const jugador2 = {
   vidaMaxima: 50,
   ataque: 20,
   clase: "Guerrero",
-}
-
-let jugadorActual=jugador;
-
-function cambiarPersonaje(){
-  if(jugadorActual===jugador){
-    jugadorActual = jugador2;
-  }else{
-    jugadorActual=jugador;
-  }
-
-  actualizarNombre();
-  escribirEnLog(`Has cambiado a ${jugadorActual.nombre}`);
-}
-
-const btnCambiarJugador = document.getElementById("btn-cambio");
-btnCambiarJugador.addEventListener("click", () => {
-  cambiarPersonaje();
-});
+};
 
 //Objeto enemigo
 const enemigo = {
@@ -81,55 +26,127 @@ const enemigo = {
   vidaMaxima: 70,
   ataque: 7,
   tipo: "bestia",
+};
+
+//Variable para saber quien esta peleando
+let jugadorActual = jugador;
+
+//Funcion para subida de nivel
+function subirNivel(jg) {
+  jg.nivel += 1;
+  jg.ataque += 5;
+  console.log(`Subiste de nivel por tanto has ganado: ${jg.ataque}`);
+  escribirEnLog(`${jg.nombre} ha subido al nivel ${jg.nivel} (Ataque +5)`);
+  actualizarStatsUI();
 }
 
-const btnLuchar = document.getElementById("btn-lucha");
-btnLuchar.addEventListener("click", () => {
-  jugador.atacar(enemigo);
-})
+//funcion para que los heroes hagan daño a el enemigo
+function atacar(jugadorActual, enemigo) {
+  //Restamos vida al enemigo
+  enemigo.vida -= jugadorActual.ataque;
 
-const btnSubirNivel = document.getElementById("levelUp");
-btnSubirNivel.addEventListener("click", () => {
-  jugador.subirNivel();
-});
+  if (enemigo.vida <= 0) {
+    enemigo.vida = 0;
+
+    escribirEnLog(
+      `¡${jugadorActual.nombre} ha derrotado a ${enemigo.nombre}! 🏆`,
+    );
+  } else {
+    escribirEnLog(
+      `${jugadorActual.nombre} ataca! a ${enemigo.nombre} le quedan ${enemigo.vida} HP`,
+    );
+  }
+
+  actualizarInterfaz();
+}
+
+function caracteristicas() {
+  console.log(
+    `| Nombre: ${jugadorActual.nombre} | Clase: ${jugadorActual.clase} |
+       |Ataque: ${jugadorActual.ataque} | Vida: ${jugadorActual.vida} |`,
+  );
+}
+
+function actualizarInterfaz() {
+  document.getElementById("nombre-jugador").innerText = jugadorActual.nombre;
+  document.getElementById("nombre-enemigo").innerText = enemigo.nombre;
+  document.getElementById("vida-enemigo").innerText = enemigo.vida;
+
+  //Actualiza la interfaz de estadisticas,Esto te asegura que mientras este abierto si subes de nivel se vera reflejado en la parte derecha al momento
+  actualizarStatsUI();
+}
+
+function cambiarPersonaje() {
+  if (jugadorActual === jugador) {
+    jugadorActual = jugador2;
+  } else {
+    jugadorActual = jugador;
+  }
+
+  actualizarInterfaz();
+  escribirEnLog(`Has cambiado a ${jugadorActual.nombre}`);
+}
 
 //Nombre del jugador que aparece en la parte de abajo a la izquierda
 function actualizarNombre() {
-  const nombreJugadorContenedor = document.getElementById("nombre-jugador");
-
-  nombreJugadorContenedor.innerText = jugadorActual.nombre.toUpperCase();
-
-  escribirEnLog(`Esta combatiendo el ${jugadorActual.nombre}`);
+  document.getElementById("nombre-jugador").innerText = jugadorActual.nombre;
 }
 
 //Actualizar la vida del enemigo
 function actualizarVidaEnemigo() {
-  const vidaEnemigoContenedor = document.getElementById("vida-enemigo");
-
-  vidaEnemigoContenedor.innerText = enemigo.vida;
-
-  escribirEnLog(`Vida de ${enemigo.nombre} : ${enemigo.vida}`);
+  document.getElementById("vida-enemigo").innerText = enemigo.vida;
 }
-
 
 //Nombre del Enemigo que aparece en la parte superior a la izquierda
 function actualizarNombreEnemigo() {
-  const nombreEnemigoContenedor = document.getElementById("nombre-enemigo");
-
-  nombreEnemigoContenedor.innerText = enemigo.nombre.toUpperCase();
-
-  escribirEnLog(`Esta combatiendo el ${enemigo.nombre}`);
+  document.getElementById("nombre-enemigo").innerText = enemigo.nombre;
 }
 
-
 //Texto de encima del HUD
-const log = document.getElementById("registro-combate");
-
 function escribirEnLog(mensaje) {
-  log.innerHTML += `<p style="color:white"> ${mensaje}</p>`;
+  const log = document.getElementById("registro-combate");
+  log.innerHTML += `<p style="color:white; margin:5px 0; font-family:monospace;">> ${mensaje}</p>`;
   log.scrollTop = log.scrollHeight;
 }
 
-actualizarNombre(jugador);
-actualizarNombreEnemigo(enemigo);
-actualizarVidaEnemigo(enemigo);
+function actualizarStatsUI() {
+  document.getElementById("stat-nombre").innerText = jugadorActual.nombre;
+  document.getElementById("stat-nivel").innerText = jugadorActual.nivel;
+  document.getElementById("stat-ataque").innerText = jugadorActual.ataque;
+  document.getElementById("stat-vida").innerText = jugadorActual.vida;
+  document.getElementById("stat-max-vida").innerText = jugadorActual.vidaMaxima;
+}
+
+//4. Eventos de los botones
+
+document.getElementById("btn-cambio").addEventListener("click", () => {
+  cambiarPersonaje();
+});
+
+document.getElementById("btn-stat").addEventListener("click", () => {
+  const panelStats = document.getElementById("stats-container");
+
+  if(panelStats){
+    panelStats.classList.toggle("oculto");
+
+  if (!panelStats.classList.contains("oculto")) {
+    actualizarStatsUI();
+    escribirEnLog("Abriendo panel de estadisticas...");
+  }
+}else{
+  console.log("Error con el stat-container");
+}
+});
+
+document.getElementById("btn-lucha").addEventListener("click", () => {
+  atacar(jugadorActual, enemigo);
+});
+
+document.getElementById("levelUp").addEventListener("click", () => {
+  subirNivel(jugadorActual);
+});
+
+//5. Iniciamos todo
+actualizarInterfaz();
+
+escribirEnLog(`Un ${enemigo.nombre} salvaje apareció`);
